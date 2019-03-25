d6b1a97c778768f5d99a02f6edd44df1aa898cd3
SkRhRasel <sk.rasel.cse@gmail.com>
2019-03-25 22:18:07 +0600

    HelloJava

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+
+    public static void main(String[] args){
+
+    System.out.println("Hello World!");
+    System.out.println("welcome to java Programming.");
+
+    }
+}
